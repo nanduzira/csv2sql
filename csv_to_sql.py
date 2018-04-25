4241e3d193154b278adc76d124cc774ac357d041
Anand Krishna Sunil <nanduzira@users.noreply.github.com>
2018-04-25 13:13:08 +0530

    Added Filepath argument

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,47 +1,58 @@
-import pandas as pd
+# import pandas as pd
 import csv
 import json
-import numpy as np
-import sqlalchemy as sa
+# import numpy as np
+# import sqlalchemy as sa
 import time
+import click
+import os
 
-FILENAME = "./datasets/title.principals.0.tsv"
-MODIFY_CSV = "./datasets/title.principals.0.csv"
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-CONFIG_JSON = "./conf/db_info.json"
-with open(CONFIG_JSON,'r') as fp:
-    db_info = json.load(fp)
-ENGINE_URL = db_info['type']+"://"+db_info['user']+":"+db_info['pass']+"@"+db_info['host']+":"+db_info['port']+"/"+db_info['name']+"?charset=utf8"
-print ENGINE_URL
-engine = sa.create_engine(ENGINE_URL, encoding="utf-8", echo=True)
-con = engine.connect()
+@click.command(context_settings=CONTEXT_SETTINGS, options_metavar='<options>')
+@click.argument('filename', metavar='<FILENAME>')
 
-with open(FILENAME, 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
-    temp_csv_list = list(spamreader)
-    del(spamreader)
-    
-    data = pd.DataFrame(temp_csv_list[1:], columns=temp_csv_list[0])
-    del(temp_csv_list)
-    print data.info()
-    print np.where(pd.isnull(data))
+def csv_2_sql(filename):
+    """ A BASIC CLI TOOL TO IMPORT STRUCTURED CSV DATA TO A DATABASE """
+    start_time = time.clock()
+    FILEPATH = os.path.join(os.getcwd(), filename)
+    print(FILEPATH)
+    # MODIFY_CSV = "./datasets/title.principals.0.csv"
 
-    data = data.replace('\N', np.NaN)
+    # CONFIG_JSON = "./conf/db_info.json"
+    # with open(CONFIG_JSON,'r') as fp:
+    #     db_info = json.load(fp)
+    # ENGINE_URL = db_info['type']+"://"+db_info['user']+":"+db_info['pass']+"@"+db_info['host']+":"+db_info['port']+"/"+db_info['name']+"?charset=utf8"
+    # print ENGINE_URL
 
-    print data.isnull().sum()
+    # engine = sa.create_engine(ENGINE_URL, encoding="utf-8", echo=True)
+    # con = engine.connect()
 
-    data.to_csv(MODIFY_CSV, index=False)
+    # with open(FILENAME, 'rb') as csvfile:
+    #     spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+    #     temp_csv_list = list(spamreader)
+    #     del(spamreader)
+        
+    #     data = pd.DataFrame(temp_csv_list[1:], columns=temp_csv_list[0])
+    #     del(temp_csv_list)
+    #     print data.info()
+    #     print np.where(pd.isnull(data))
 
-chunks = pd.read_csv(MODIFY_CSV, chunksize=100000, index_col=0)
+    #     data = data.replace('\N', np.NaN)
 
-start_time = time.clock()
-TABLE_NAME = "TITLE_PRINCIPALS"
-try:
-    for chunk in chunks:
-        print chunk
-        chunk.to_sql(name=TABLE_NAME, con=con, if_exists="append")
-    con.close()
-except Exception, e:
-    print "EXCEPTION", e
+    #     print data.isnull().sum()
 
-print time.clock() - start_time, "seconds"
+    #     data.to_csv(MODIFY_CSV, index=False)
+
+    # chunks = pd.read_csv(MODIFY_CSV, chunksize=100000, index_col=0)
+
+    # TABLE_NAME = "TITLE_PRINCIPALS"
+    # try:
+    #     for chunk in chunks:
+    #         print chunk
+    #         chunk.to_sql(name=TABLE_NAME, con=con, if_exists="append")
+    #     con.close()
+    # except Exception, e:
+    #     print "EXCEPTION", e
+
+    print("Transfer completed in {0:.6f} seconds>".format(time.clock() - start_time))
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="csv2sql",
     version='0.1',
-    py_modules=['hello'],
+    py_modules=['csv_to_sql'],
     install_requires=[
-        'Click', 'pandas', ''
+        'Click', 'pandas', 'numpy', 'sqlalchemy'
     ],
     entry_points='''
         [console_scripts]
-        csv2sql=hello:cli
+        csv2sql=csv_to_sql:csv_2_sql
     ''',
 )
